be955e975f99b996a87d82fe35bf85edc6254381
dt440 <2391899438@qq.com>
2024-10-06 21:52:42 +0800

    计算圆柱体

---
+++ 计算圆柱体/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        double r=scanner.nextDouble();
+        double h=scanner.nextDouble();
+        Circle c = new Circle(r);
+        Column column=new Column();
+        column.setBottom(c);
+        column.setHeight(h);
+        System.out.printf("底面面积和周长分别为：%.2f %.2f\n",column.getBottom().getArea(),column.getBottom().getLength());
+        System.out.printf("体积为：%.2f\n",column.getVolume());
+        scanner.close();
+    }
+}
+class Circle{
+    double radius,Height;
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+    public double getLength(){
+        return 2*Math.PI*radius;
+    }
+}
+class Column{
+    private Circle bottom;
+    private double Height;
+
+    public void setBottom(Circle bottom) {
+        this.bottom = bottom;
+    }
+
+    public Circle getBottom() {
+        return bottom;
+    }
+
+    public void setHeight(double height) {
+        Height = height;
+    }
+
+    public double getHeight() {
+        return Height;
+    }
+    public double getVolume(){
+        return Height*bottom.getArea();
+    }
+}
